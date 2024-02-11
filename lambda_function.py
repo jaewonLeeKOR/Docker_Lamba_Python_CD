c7c8adbaf66f00e9a65df80d4122979be5fa2415
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2024-02-11 21:00:23 +0900

    chore : lambda_function.py change return

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,5 @@
 def lambda_handler(event, context):
-    message = 'Hello {} {}!'.format(event['first_name'], event['last_name'])  
+    message = 'Bye {} {}!'.format(event['first_name'], event['last_name'])  
     return { 
         'message' : message
     }
